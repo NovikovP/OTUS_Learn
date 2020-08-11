27a5e93980f84c1f3d8f6d7d83a51398a3e9a745
Novikov Pavel <successdca@gmail.com>
2020-08-11 20:12:38 +0300

    Init repo

---
+++ 001_Lesson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _001_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
